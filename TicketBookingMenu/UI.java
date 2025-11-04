e901bdfeb679aad257d85f522fe36ba36e403ff6
AbhijithSuresh-USTHP <302868@ust.com>
2025-11-04 15:18:26 +0530

    Update UI.java

--- TicketBookingMenu/UI.java
+++ TicketBookingMenu/UI.java
@@ -6,7 +6,7 @@ public class UI {
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		while(true) {
-		System.out.println("\nEnter your choice:\n1.Add customer\n2.View customers\n3.Remove customers\n4.Add movies\n5.View movies\n6.Remove movies\n7.Book tickets\n8.View all the tickets that has been purchased\n9.View booking history\n10.Exit");
+		System.out.println("\nEnter your choice:\n1.Add customer\n2.View customers\n3.Remove customers\n4.Add movies\n5.View movies\n6.Remove movies\n7.Book tickets\n8.View all the tickets that has been purchased(Admin Option)\n9.View booking history\n10.Exit");
 		int ch = sc.nextInt();
 		switch(ch) {
 		case 1:{
